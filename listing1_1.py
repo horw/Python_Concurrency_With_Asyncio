394e66fdf89e6afaab3125bca4998800303e1489
Horw <horw7001@gmail.com>
2022-07-04 21:37:31 +0300

    listing 1.1

---
+++ listing1_1.py
@@ -0,0 +1,12 @@
+import requests
+
+response = requests.get('https://www.example.com')
+
+items = response.headers.items()
+
+headers = [f'{key}: {headers}' for key,headers in items]
+
+formated_headers = '\n'.join(headers)
+
+with open('headers.txt', 'w+') as file:
+    file.write(formated_headers)
---
+++ main.py
@@ -0,0 +1,1 @@
+import listing1_1
