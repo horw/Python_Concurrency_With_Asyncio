6ec61a61e1d7c6374b8ac83175012b3f0828dcdd
Horw <horw7001@gmail.com>
2022-07-05 22:58:26 +0300

    listing 1.5

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 #import listing1_1
 #import listing1_2
 #import listing1_3
-import listing1_4
-
+#import listing1_4
+import listing1_5
 
 if __name__ == '__main__':
-    print('Start Main ....')
+    pass
+    #print('Start Main ....')
     #listing1_4.list1_4()
